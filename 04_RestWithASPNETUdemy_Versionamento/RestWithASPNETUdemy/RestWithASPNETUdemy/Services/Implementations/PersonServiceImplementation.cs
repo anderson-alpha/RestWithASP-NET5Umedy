208fcb79c1f90962af3b45d3f8ef286479ed5696
anderson.sousa <anderson.sousa@tripletech.com.br>
2022-02-23 08:47:38 -0300

    Session9

---
+++ 04_RestWithASPNETUdemy_Versionamento/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,121 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        //SERVE PARA MOCKAR VALOR
+        //private volatile int count;
+        private MySQLContext _context;
+
+        public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public List<Person> FindAll()
+        {
+            //List<Person> persons = new List<Person>();
+            //for (int i = 0; i <8;i++) 
+            //{
+
+            //    Person person = MockPerson(i);
+            //    persons.Add(person);
+            //}
+
+            return _context.Persons.ToList();
+        }
+
+        //private Person MockPerson(int i)
+        //{
+        //    return new Person
+        //    {
+        //        Id = 1, //IncrementAndGet(),
+        //        FirstName = "Person Name" + i,
+        //        LastName = "Person Last Name" + i,
+        //        Address = "Person Address" + i,
+        //        Gender = "Person Male" + i
+        //    };
+        //}
+
+        //private long IncrementAndGet()
+        //{
+        //    return Interlocked.Increment(ref count);
+        //}
+
+        public Person FindByID(long id)
+        {
+            //return new Person
+            //{
+            //    Id = 1, //IncrementAndGet(),
+            //    FirstName = "Anderson",
+            //    LastName = "Sousa",
+            //    Address = "Rua Principal",
+            //    Gender = "Male"
+            //};
+
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception) 
+                {
+                    throw;
+                }
+            }
+            return person;
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+    }
+}
---
+++ 05_RestWithASPNETUdemy_Camadas/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindByID(id);
+            if(person ==null) return NotFound();
+
+            return Ok(person);
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {            
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+
+            return NoContent();
+        }
+
+        //[HttpGet("sum/{firstNumber}/{secondNumber}")]
+        //public IActionResult Sum(string firstNumber, string secondNumber)
+        //{
+        //    //if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        //    //{
+        //    //    var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+        //    //    return Ok(sum.ToString());
+        //    //}
+        //    return BadRequest("invalid Input");
+        //}
+
+
+        //[HttpGet("sub/{firstNumber}/{secondNumber}")]
+        //public IActionResult Sub(string firstNumber, string secondNumber)
+        //{
+        //    if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        //    {
+        //        var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+        //        return Ok(sub.ToString());
+        //    }
+        //    return BadRequest("invalid Input");
+        //}
+
+        //[HttpGet("div/{firstNumber}/{secondNumber}")]
+        //public IActionResult Div(string firstNumber, string secondNumber)
+        //{
+        //    if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        //    {
+        //        var div = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+        //        return Ok(div.ToString());
+        //    }
+        //    return BadRequest("invalid Input");
+        //}
+
+        //[HttpGet("mult/{firstNumber}/{secondNumber}")]
+        //public IActionResult Mult(string firstNumber, string secondNumber)
+        //{
+        //    if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        //    {
+        //        var mult = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+        //        return Ok(mult.ToString());
+        //    }
+        //    return BadRequest("invalid Input");
+        //}
+
+        //[HttpGet("mean/{firstNumber}/{secondNumber}")]
+        //public IActionResult Mean(string firstNumber, string secondNumber)
+        //{
+        //    if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        //    {
+        //        var mean = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber))/2;
+        //        return Ok(mean.ToString());
+        //    }
+        //    return BadRequest("invalid Input");
+        //}
+
+        //[HttpGet("Sqr/{firstNumber}")]
+        //public IActionResult Sqr(string firstNumber)
+        //{
+        //    if (IsNumeric(firstNumber))
+        //    {
+        //        var sqr = Math.Sqrt((double)ConvertToDecimal(firstNumber));
+        //        return Ok(sqr.ToString());
+        //    }
+        //    return BadRequest("invalid Input");
+        //}
+
+        //private decimal ConvertToDecimal(string strNumber)
+        //{
+        //    decimal decimalValue;
+
+        //    if (decimal.TryParse(strNumber, out decimalValue))
+        //    {
+        //        return decimalValue;
+        //    }
+        //    return 0;
+        //}
+
+        //private bool IsNumeric(string strNumber)
+        //{
+        //    double number;
+
+        //    bool isNumber = double.TryParse(
+        //        strNumber,
+        //        System.Globalization.NumberStyles.Any,
+        //        System.Globalization.NumberFormatInfo.InvariantInfo,
+        //        out number);
+
+        //    return isNumber;
+        //}
+    }
+}
