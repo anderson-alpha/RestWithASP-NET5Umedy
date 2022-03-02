41aeced6781fecccf28ba160f9af15c0d197ad0f
anderson.sousa <anderson.sousa@tripletech.com.br>
2022-03-02 20:25:11 -0300

    Session19-Autenticação

---
+++ 14_RestWithASPNETUdemy_AUTENTICACAO/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/UserVO.cs
@@ -0,0 +1,12 @@
+﻿using RestWithASPNETUdemy.Hypermedia;
+using System;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class UserVO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
