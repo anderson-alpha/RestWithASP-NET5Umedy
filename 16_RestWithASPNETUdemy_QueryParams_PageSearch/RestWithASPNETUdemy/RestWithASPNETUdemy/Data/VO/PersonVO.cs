324d946b5fadc55b1c8f9a3fbe5ce1d022077cb2
anderson.sousa <anderson.sousa@tripletech.com.br>
2022-03-04 21:50:26 -0300

    Session21-QueryParamsPAgination

---
+++ 16_RestWithASPNETUdemy_QueryParams_PageSearch/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,23 @@
+﻿using RestWithASPNETUdemy.Hypermedia;
+using RestWithASPNETUdemy.Hypermedia.Abstract;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO: ISupportsHyperMedia
+    {   
+        //[JsonPropertyName("code")]
+        public  long Id { get; set; }
+        //[JsonPropertyName("Nome")]
+        public string FirstName { get; set; }
+        //[JsonPropertyName("Sobrenome")]
+        public string LastName { get; set; }
+        //[JsonPropertyName("Endereco")]
+        public string Address { get; set; }
+        //[JsonIgnore] //EXCLUI DA VISUALIZACAO
+        public string Gender { get; set; }
+        public bool Enabled { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
