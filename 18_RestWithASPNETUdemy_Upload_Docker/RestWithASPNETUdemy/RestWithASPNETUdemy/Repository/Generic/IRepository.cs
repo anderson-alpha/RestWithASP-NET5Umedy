68d22e3389a268020120089a88681d1f22b91d9c
anderson.sousa <anderson.sousa@tripletech.com.br>
2022-03-07 21:37:12 -0300

    Session23-Travis

---
+++ 18_RestWithASPNETUdemy_Upload_Docker/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Generic/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public interface IRepository<T> where T : BaseEntity /*Heranca: herda a estrutura de uma tabela, no caso o Id*/
+    {
+        T Create(T item);
+        T FindByID(long id);
+        List<T> FindAll();
+        T Update(T item);
+        void Delete(long id);
+        bool Exists(long id);
+        List<T> FindWithPagedSearch(string query);
+        int GetCount(string query);
+    }    
+}
