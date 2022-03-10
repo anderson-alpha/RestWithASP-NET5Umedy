f98f58c9e8a1bf22a9c84ab320fde753a1b9c366
anderson.sousa <anderson.sousa@tripletech.com.br>
2022-03-10 20:22:59 -0300

    Session24-Azure

---
+++ 19_RestWithASPNETUdemy_Azure/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNETUdemy.Hypermedia;
+using RestWithASPNETUdemy.Hypermedia.Abstract;
+using System;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+
+    public class BookVO : ISupportsHyperMedia
+    {
+        public  long Id { get; set; }        
+        public string Author { get; set; }
+        public DateTime LaunchDate { get; set; }
+        public float Price { get; set; }
+        public string Title { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
